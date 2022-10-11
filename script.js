019484a2f0bfdde809e616ba0a715d60300c7bf5
Hassan Rafiq <hassanrafiq321.hr@gmail.com>
2022-10-12 03:33:09 +0500

    C to clear all

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ Array.from(button).forEach((button) => {
         string = eval(string);
         document.querySelector('input').value = string;
     }
+    else if(e.target.innerHTML == 'C'){
+        string = "";
+        document.querySelector('input').value = string;
+    }
     else{
     console.log(e.target);
     string = string + e.target.innerHTML;
